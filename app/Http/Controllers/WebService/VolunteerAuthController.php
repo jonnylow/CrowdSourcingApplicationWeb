2e8a688a9955620088b0d4d0cbb029717da9129e
changhuapeng <hpchang.2013@sis.smu.edu.sg>
2016-01-12 23:57:13 +0800

    Update web service with mobile user authentication using JWT token.

--- app/Http/Controllers/WebService/VolunteerAuthController.php
+++ app/Http/Controllers/WebService/VolunteerAuthController.php
@@ -14,6 +14,9 @@ class VolunteerAuthController extends Controller
 {
     public function __construct()
    {
+       // Set the Eloquent model that should be used to retrieve your users
+       Config::set('auth.model', 'App\Volunteer');
+
        // Apply the jwt.auth middleware to all methods in this controller
        // except for the authenticate method. We don't want to prevent
        // the user from retrieving their token if they don't already have it
@@ -27,22 +30,44 @@ class VolunteerAuthController extends Controller
      */
     public function authenticate(Request $request)
     {
-        Config::set('auth.model', 'App\Volunteer');
-
         // grab credentials from the request
         $credentials = $request->only('email', 'password');
 
         try {
             // attempt to verify the credentials and create a token for the user
             if (! $token = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'invalid_credentials'], 401);
+                return response()->json(['error' => 'invalid_credentials']);
             }
         } catch (JWTException $e) {
             // something went wrong whilst attempting to encode the token
-            return response()->json(['error' => 'could_not_create_token'], 500);
+            return response()->json(['error' => 'could_not_create_token']);
         }
 
+        // get user's detail to include with returning token
+        $authenticatedUser = JWTAuth::setToken($token)->authenticate()->toArray();
+        $user = array_only($authenticatedUser, ['volunteer_id', 'name', 'email', 'is_approved']);
+
         // all good so return the token
-        return response()->json(compact('token'));
+        return response()->json(compact('token', 'user'));
+    }
+
+    // Retrieving the Authenticated user from a token
+    // which will automatically get the token from the request
+    public function getAuthenticatedUser()
+    {
+        try {
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['error' => 'user_not_found'], 404);
+            }
+        } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+            return response()->json(['error' => 'token_expired'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+            return response()->json(['error' => 'token_invalid'], $e->getStatusCode());
+        } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+            return response()->json(['error' => 'token_absent'], $e->getStatusCode());
+        }
+
+        // the token is valid and we have found the user via the sub claim
+        return response()->json(compact('user'));
     }
 }
