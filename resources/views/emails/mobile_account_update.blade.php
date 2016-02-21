db1f0e9fc00b09edb29b35928c952ee42bebdaf1
changhuapeng <hpchang.2013@sis.smu.edu.sg>
2016-02-21 18:15:50 +0800

    Fix mailto links for all emails content.

--- resources/views/emails/mobile_account_update.blade.php
+++ resources/views/emails/mobile_account_update.blade.php
@@ -4,5 +4,5 @@
 <br>
 <p>If you have initiated this request, you don't need to take any further action and can safely disregard this email.</p>
 <br>
-<p>Should you ever encounter problems, please contact us at <a href="imchosen6@gmail.com">imchosen6@gmail.com</a>.</p>
+<p>Should you ever encounter problems, please contact us at <a href="mailto:imchosen6@gmail.com">imchosen6@gmail.com</a>.</p>
 <p>This is a system generated email so please do not reply to this email.</p>
