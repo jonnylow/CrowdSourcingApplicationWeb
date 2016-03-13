7e4dc5744851d76c43f21dc4d3a00a772659d862
changhuapeng <hpchang.2013@sis.smu.edu.sg>
2016-03-13 11:08:32 +0800

    Change UI for viewing user profile.

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('title', 'View your Profile')
+@section('title', 'View my Profile')
 
 @section('content')
 
@@ -9,22 +9,27 @@
         <div class="col-md-8 col-md-offset-2">
             <h1>View my Profile</h1>
 
-            {!! Form::model(Auth::user(), ['class' => 'form-horizontal']) !!}
-                <!-- Name Form Input -->
-                <div class="form-group">
-                    {!! Form::label('name', 'Name', ['class' => 'control-label col-md-4']) !!}
-                    <div class="col-md-8">
-                        {!! Form::text('name', null, ['class' => 'form-control', 'readonly']) !!}
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="heading-information">
+                        <h4 class="panel-title">
+                            <a role="button" data-toggle="collapse" href="#collapse-information" aria-expanded="true" aria-controls="collapse-information">
+                                <span class="fa fa-fw fa-user"></span>
+                                <strong>Personal Information</strong>
+                                <span class="icon-arrow fa fa-lg fa-chevron-up"></span>
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapse-information" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading-information">
+                        <div class="panel-body">
+                            <dl class="dl-horizontal">
+                                <dt>Name:</dt><dd>{{ $profile->name }}</dd>
+                                <dt>Email:</dt><dd>{{ $profile->email }}</dd>
+                            </dl>
+                        </div>
                     </div>
                 </div>
-                <!-- Email Form Input -->
-                <div class="form-group">
-                    {!! Form::label('email', 'Email', ['class' => 'control-label col-md-4']) !!}
-                    <div class="col-md-8">
-                        {!! Form::email('email', null, ['class' => 'form-control', 'readonly']) !!}
-                    </div>
-                </div>
-            {!! Form::close() !!}
+            </div>
         </div>
     </div>
 </div>
