1e9ac837011659add2818c0d0c0b8859f8f32e2b
changhuapeng <hpchang.2013@sis.smu.edu.sg>
2016-04-01 02:57:56 +0800

    Remove unnecessary JS code for activating bootstrap tabs.

--- resources/views/activities/index.blade.php
+++ resources/views/activities/index.blade.php
@@ -168,11 +168,6 @@
         $('#upcoming .fixed-table-toolbar .search input').attr('placeholder', 'Search upcoming activities');
     });
 
-    $('#activityTabs a').click(function (e) {
-        e.preventDefault();
-        $(this).tab('show');
-    });
-
     // store the currently selected tab in the hash value
     $("ul.nav-tabs > li > a").on('shown.bs.tab', function (e) {
         var id = $(e.target).attr('href').substr(1);
